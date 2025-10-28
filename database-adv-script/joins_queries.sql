b55a7aa453442ca2eca6896b9d3d7665eec8f65e
pius wanyangu <ndubipius96@gmail.com>
2025-10-28 09:39:34 +0300

    adding joins-queries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,10 +1,10 @@
-Write a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+
 
 select *
 from bookings inner join users
 on bookings.user_id = users.user_id;
 
-Write a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
+
 
 
 
@@ -13,7 +13,7 @@ from properties left join reviews
 on properties.property_id = reviews.property_id;
 
 
-Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
 
 select *
 from users left join bookings
