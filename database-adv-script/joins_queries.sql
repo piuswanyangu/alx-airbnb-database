84471faf75e59b4bf59b2356b555f5211913cc19
pius wanyangu <ndubipius96@gmail.com>
2025-10-28 08:59:29 +0300

    adding joins_querry

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,19 @@
-# not empty
+-- inner join querries on user and bookings
+select *
+from bookings inner join users
+on booking.user_id = users.user_id;
+
+-- Left join properties and reviews
+select *
+from properties left join reviews
+on properties.property_id = reviews.property_id;
+
+-- full outer join for users and bookings
+select *
+from users left join bookings
+on users.user_id = bookings.user_id
+union
+
+select * 
+from users right join bookings
+on users.user_id = bookings.user_id;
