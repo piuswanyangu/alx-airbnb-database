7dfcbd551b5741fbaa3dd8067c39ad5801b5e2d5
pius wanyangu <ndubipius96@gmail.com>
2025-10-29 20:29:36 +0300

    adding partition peromance

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,4 +1,4 @@
-alter table bookings
+alter table bookings_table
 partition by range (year(start_date))(
     partition p2023 values less than (2023)
     partition p2024 values less than (2024)
@@ -9,12 +9,12 @@ partition by range (year(start_date))(
 -- queery before partitioning 
 EXPLAIN 
 SELECT *
-FROM bookings
+FROM bookings_table
 WHERE start_date BETWEEN '2023-01-01' AND '2023-12-31';
 
 
 -- querry  after partitioning 
 EXPLAIN
 SELECT * 
-FROM bookings
+FROM bookings_table
 WHERE year(start_date)=2024;
